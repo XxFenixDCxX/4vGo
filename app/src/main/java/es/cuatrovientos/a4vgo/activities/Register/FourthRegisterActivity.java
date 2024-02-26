f37347e92f07ba025f330b7fa03092ca069440a4
Arkaitz Calvo Saldias <fenixdc7@gmail.com>
2024-02-26 20:52:33 +0100

    Creada 4 activida de registro vacia

---
+++ app/src/main/java/es/cuatrovientos/a4vgo/activities/Register/FourthRegisterActivity.java
@@ -0,0 +1,16 @@
+package es.cuatrovientos.a4vgo.activities.Register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.cuatrovientos.a4vgo.R;
+
+public class FourthRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth_register);
+    }
+}
