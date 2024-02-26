b3a5352691bca6de4cd5bc5a20edebce59b1805c
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2024-02-26 11:12:20 +0100

    Agregado diseño y logica de pantalla main y agregado diseño de pantalla register

--- app/src/main/java/es/cuatrovientos/a4vgo/MainActivity.java
+++ app/src/main/java/es/cuatrovientos/a4vgo/MainActivity.java
@@ -1,8 +1,11 @@
 package es.cuatrovientos.a4vgo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConstraintLayout constraintLayout1 = findViewById(R.id.constraintLayout3); // Reemplaza R.id.constraintLayout con el ID real de tu ConstraintLayout
+
+        constraintLayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Abre la actividad asociada a constraintLayout2 al hacer clic en el ConstraintLayout
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
