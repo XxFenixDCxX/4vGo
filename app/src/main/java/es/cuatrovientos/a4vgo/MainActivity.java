d134e34f0fcb08dc54dae56dcd01ada09dec5a35
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2024-02-26 10:32:29 +0100

    Creacion del proyecto

---
+++ app/src/main/java/es/cuatrovientos/a4vgo/MainActivity.java
@@ -0,0 +1,14 @@
+package es.cuatrovientos.a4vgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
