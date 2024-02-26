937b941a3a94a45ea4cf576a60f50b8e3eb85ace
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2024-02-26 10:43:58 +0100

    Agregada implementacion de Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,8 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
