eff3513348ff6f3ae53b5c18b123eb493736bc63
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2024-02-27 14:14:36 +0100

    Agregado auto rellenado de datos de perfil y solucionado problemas con upper y lower cases

---
+++ app/src/main/java/es/cuatrovientos/a4vgo/activities/profile/PersonalDetailsActivity.java
@@ -0,0 +1,15 @@
+package es.cuatrovientos.a4vgo.activities.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.cuatrovientos.a4vgo.R;
+
+public class PersonalDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details);
+    }
+}
--- app/src/main/java/es/cuatrovientos/a4vgo/activities/profile/ProfileActivity.java
+++ app/src/main/java/es/cuatrovientos/a4vgo/activities/profile/ProfileActivity.java
@@ -5,24 +5,61 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import es.cuatrovientos.a4vgo.R;
+import es.cuatrovientos.a4vgo.Utils.DialogUtils;
 import es.cuatrovientos.a4vgo.activities.MainActivity;
 
 public class ProfileActivity extends AppCompatActivity {
-    ConstraintLayout logout;
+    ConstraintLayout logout, personalDetails;
+    CollectionReference collection;
+    FirebaseUser currentUser;
+    TextView username, email;
+    Query query;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         logout = findViewById(R.id.logoutAction);
+        personalDetails = findViewById(R.id.personalDetailsAction);
+        collection = FirebaseFirestore.getInstance().collection("personalDetails");
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        username = findViewById(R.id.txtUsername);
+        email = findViewById(R.id.txtEmailProfile);
+        query = collection.whereEqualTo("email", currentUser.getEmail());
+
+        query.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                QuerySnapshot querySnapshot = task.getResult();
+                DocumentSnapshot document = querySnapshot.getDocuments().get(0);
+                String usernameString = document.getString("username");
+                assert usernameString != null;
+                usernameString = usernameString.toUpperCase().charAt(0) + usernameString.substring(1);
+                username.setText(usernameString);
+                email.setText(document.getString("email"));
+            } else {
+                DialogUtils.showErrorDialog(this, getString(R.string.errorLogCantLoadDetails), getString(R.string.errorLoginMessage));
+            }
+        });
 
         logout.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
             Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        personalDetails.setOnClickListener(v -> {
+            Intent intent = new Intent(ProfileActivity.this, PersonalDetailsActivity.class);
             startActivity(intent);
         });
     }
