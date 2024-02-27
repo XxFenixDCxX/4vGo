33470cb887e17f22a788fbed4e222c4343374d94
Mohammed Choudna <medc7202@gmail.com>
2024-02-27 09:15:57 +0100

    Crear la pantalla de buscador de rutas

---
+++ app/src/main/java/es/cuatrovientos/a4vgo/activities/MainRoutesActivity.java
@@ -0,0 +1,16 @@
+package es.cuatrovientos.a4vgo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.cuatrovientos.a4vgo.R;
+
+public class MainRoutesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_routes);
+    }
+}
--- app/src/main/java/es/cuatrovientos/a4vgo/activities/loginActivity.java
+++ app/src/main/java/es/cuatrovientos/a4vgo/activities/loginActivity.java
@@ -2,9 +2,13 @@ package es.cuatrovientos.a4vgo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import es.cuatrovientos.a4vgo.R;
+import es.cuatrovientos.a4vgo.activities.Register.FirstRegisterActivity;
 
 public class loginActivity extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class loginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btn = findViewById(R.id.buttonLogin);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(loginActivity.this, MainRoutesActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
