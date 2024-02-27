64e9a68496c53880c170bfff2553b482fb2fb794
Mohammed Choudna <medc7202@gmail.com>
2024-02-27 12:35:33 +0100

    Crear la pantalla de añadir ruta

--- app/src/main/java/es/cuatrovientos/a4vgo/activities/MainRoutesActivity.java
+++ app/src/main/java/es/cuatrovientos/a4vgo/activities/MainRoutesActivity.java
@@ -2,15 +2,56 @@ package es.cuatrovientos.a4vgo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import es.cuatrovientos.a4vgo.R;
+import es.cuatrovientos.a4vgo.activities.CreateRoute.AddRouteActivity;
 
 public class MainRoutesActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_routes);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(
+                item -> {
+                    int id = item.getItemId();
+                    if (id == R.id.trips) {
+
+                        return true;
+                    } else if (id == R.id.trips) {
+                        overridePendingTransition(0, 0);
+
+                        Intent intent = new Intent(MainRoutesActivity.this , AddRouteActivity.class);
+                        startActivity(intent);
+
+                        overridePendingTransition(0, 0);
+
+                        return true;
+                    } else if (id == R.id.profile) {
+                        overridePendingTransition(0, 0);
+
+                        Intent intent = new Intent(MainRoutesActivity.this , AddRouteActivity.class);
+                        startActivity(intent);
+
+                        overridePendingTransition(0, 0);
+
+                        return true;
+                    }
+
+
+
+                    else {
+                        return false;
+                    }
+                }
+        );
+
     }
 }
