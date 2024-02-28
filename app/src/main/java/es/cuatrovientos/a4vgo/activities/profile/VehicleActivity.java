ab54bc0d6090ec0f4c33c148203fd9208dbce986
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2024-02-28 13:52:39 +0100

    Finalizada logica de mostrar vehiculos y agregada funcionalidad a boton de agregar vehiculo, queda hacer pantalla de agregar vehiculo

--- app/src/main/java/es/cuatrovientos/a4vgo/activities/profile/VehicleActivity.java
+++ app/src/main/java/es/cuatrovientos/a4vgo/activities/profile/VehicleActivity.java
@@ -1,15 +1,22 @@
 package es.cuatrovientos.a4vgo.activities.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import es.cuatrovientos.a4vgo.R;
 import es.cuatrovientos.a4vgo.adapters.RecyclerVehicleAdapter;
 
 public class VehicleActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    Button back;
+    FloatingActionButton add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +24,20 @@ public class VehicleActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.rvVehicle);
         RecyclerVehicleAdapter adapter = new RecyclerVehicleAdapter();
+        back = findViewById(R.id.btnBackVehicle);
+        add = findViewById(R.id.fabAddVehicle);
 
         recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
+
+        back.setOnClickListener(v -> {
+            Intent intent = new Intent(VehicleActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        });
+
+        add.setOnClickListener(v -> {
+            Intent intent = new Intent(VehicleActivity.this, AddVehicleActivity.class);
+            startActivity(intent);
+        });
     }
 }
