37dd4802b55ec3a8b1bd7b8dd3cb78b6f61d213f
Arkaitz Calvo Saldias <fenixdc7@gmail.com>
2024-02-28 15:43:12 +0100

    Arreglado problemas con strings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,10 +46,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.android.gms:play-services-auth:21.0.0")
-    implementation ("com.squareup.picasso:picasso:2.71828")
     implementation("com.google.firebase:firebase-storage")
     //map
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -57,5 +56,5 @@ dependencies {
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
