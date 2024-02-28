01e595c56faaebc1d08b90b9e5e350ccfe5672f8
Mohammed Choudna <medc7202@gmail.com>
2024-02-28 14:20:34 +0100

    Crear mapas , adaptadores de spinners

---
+++ app/src/main/java/es/cuatrovientos/a4vgo/maps/searchMapActivity.java
@@ -0,0 +1,16 @@
+package es.cuatrovientos.a4vgo.maps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.cuatrovientos.a4vgo.R;
+
+public class searchMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_map);
+    }
+}
---
+++ app/src/main/java/es/cuatrovientos/a4vgo/tasks/OpenStreetMapSearchTask.java
@@ -0,0 +1,60 @@
+package es.cuatrovientos.a4vgo.tasks;
+
+import android.os.AsyncTask;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class OpenStreetMapSearchTask extends AsyncTask<String, Void, String> {
+
+    private EditText editTextOrigin;
+
+    public OpenStreetMapSearchTask(EditText editTextOrigin) {
+        this.editTextOrigin = editTextOrigin;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String streetName = params[0];
+
+        try {
+            streetName = URLEncoder.encode(streetName, "UTF-8");
+            String apiUrl = "https://nominatim.openstreetmap.org/search?format=json&q=" + streetName + "&country=USA";
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            reader.close();
+            return response.toString();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        // Aquí puedes manejar el resultado de la búsqueda
+        // El resultado es un JSON que contiene información sobre la dirección encontrada
+        // Puedes parsear el JSON y mostrar la información relevante en tu aplicación
+    }
+}
